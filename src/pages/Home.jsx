24d586d6416d0092fdcdbb5277a4fbfa93516ec9
NhatHONGUYEN <hnnhat67@gmail.com>
2024-06-12 12:37:48 +0200

    ✨   F01 : Add Home and his pic and title and presentation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,26 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="relative -z-10">
+        <img
+          className="w-full"
+          src="/photoshoot_for_baramey_piquant.jpg"
+          alt="photoshoot_for_baramey_piquant.jpg"
+        />
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="absolute inset-0 flex flex-col items-center  text-center mx-10 text-white justify-center">
+          <h1 className="text-4xl">
+            Traiteur spécialisé en plats cambodgiens et thaï
+          </h1>
+          <p>
+            Venez goûter à nos plats fait maison pour vous apporter un voyage
+            culinaire dans le sud-est asiatique grâce à un savoir-faire
+            familiale et artisanale !
+          </p>
+          <p>Découvrez le restaurant</p>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
 }
