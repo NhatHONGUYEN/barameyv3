af5912e5451548bf214a82702132ade8b037f599
NhatHONGUYEN <hnnhat67@gmail.com>
2024-06-12 17:59:04 +0200

    💄     F01 : change the size and the margin of the typo

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -8,14 +8,14 @@ export default function Introduction() {
           autoPlay
           loop
           controls={false}
-          className="object-cover w-full h-[500px]"
+          className="object-cover w-full h-[500px] sm:h-[600px] md:h-[700px] lg:h-[800px] xl:h-[900px]"
         ></video>
         <div className="absolute inset-0 bg-black opacity-70"></div>
-        <div className="absolute inset-0 flex flex-col py-11 items-center text-center mx-10 text-white justify-center">
-          <h1 className="text-xl py-2 uppercase font-extrabold font-gagalin">
+        <div className="absolute inset-0 flex flex-col py-11 items-center text-center text-white justify-center">
+          <h1 className="text-2xl mx-20  py-4 uppercase font-extrabold font-gagalin">
             Traiteur spécialisé en plats cambodgiens et thaï
           </h1>
-          <p className="py-2">
+          <p className="py-2 mx-10">
             Venez goûter à nos plats fait maison pour vous apporter un voyage
             culinaire dans le sud-est asiatique grâce à un savoir-faire
             familiale et artisanale !
