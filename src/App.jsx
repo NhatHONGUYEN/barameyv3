3d49ba48562bc313b7abd154733feaaae082c1a4
NhatHONGUYEN <hnnhat67@gmail.com>
2024-06-12 12:09:13 +0200

    ✨ F01 : Add burgerMenu with slide in and out animation and add barameyorange color

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="bg-red-500 text-white">hello</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/error" element={<Error />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "baramey-orange": "#F7A11F",
+      },
+    },
   },
   plugins: [],
 };
