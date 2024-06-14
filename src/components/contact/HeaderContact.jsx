6d42ae9014b38406bbc9c241a077ded3a15bd225
NhatHONGUYEN <hnnhat67@gmail.com>
2024-06-14 11:17:24 +0200

    📱  F02 :HeaderContact > finish the responsive

--- src/components/contact/HeaderContact.jsx
+++ src/components/contact/HeaderContact.jsx
@@ -1,27 +1,26 @@
-import FadeInOut from "../../animation/FadeInOut";
-
+import FadeInOutInstant from "../../animation/FadeInOutInstant.jsx";
 export default function HeaderContact() {
   return (
     <div className="relative">
-      <FadeInOut>
-        <div className="absolute inset-0 bg-black opacity-70"></div>
+      <div className="absolute inset-0 bg-black opacity-70"></div>
+      <img
+        src="/girlphone.jpg"
+        alt="girlphone"
+        className="w-full h-full md:h-screen   object-cover xl:top-16"
+      />
 
-        <img src="/girlphone.jpg" alt="girlphone" className="w-full" />
-      </FadeInOut>
-      <FadeInOut>
-        <div className="absolute inset-0 flex items-center justify-center text-center text-white">
-          <div className="mx-8">
-            <h1 className="text-3xl p-4 font-semibold">
-              Découvrez les saveurs authentiques du Sud-Est de l'Asie à travers
-              nos plats! !
-            </h1>
-            <p className="text-xl  p-4">
-              Commandez dès maintenant à emporter , <span>uniquement</span> par
-              téléphone.
-            </p>
-          </div>
+      <FadeInOutInstant>
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white p-8 xl:w-2/3 xl:mx-auto gap-12   ">
+          <h1 className="text-3xl md:text-4xl md:mx-20 lg:text-5xl font-semibold mb-42 xl:text-6xl 2xl:w-3/5">
+            Découvrez les saveurs authentiques du Sud-Est de l'Asie à travers
+            nos plats! !
+          </h1>
+          <p className="py-2 mx-10 md:text-2xl lg:text-4xl xl:mx-24 ">
+            Commandez dès maintenant à emporter, <span>uniquement</span> par
+            téléphone.
+          </p>
         </div>
-      </FadeInOut>
+      </FadeInOutInstant>
     </div>
   );
 }
